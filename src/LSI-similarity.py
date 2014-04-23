68b6164582b59218aa08c359704ec12c156b4782
prashanti <manda.prashanti@gmail.com>
2014-04-23 10:49:46 -0400

    Changed how stop words are handled
    Stop words are stemmed and are compared to stems of the words in the corpus. If the stem of a word matches a stem of a stopword, it is pruned. Doesn't have to be exact matches to the stop words

--- src/LSI-similarity.py
+++ src/LSI-similarity.py
@@ -1,76 +1,69 @@
 
-
-def main():
-	################### Creating a set of stopwords
+def createstoplist():
 	stoplist=set()
 	for line in open('../Evolution2014Data/stopwords.txt'):
 		line=line.strip()
 		stoplist.add(line.lower())
-	###################
+	stoplist=[stem(word) for word in stoplist]
+	return(stoplist)
 
+def filterstopwords(stoplist):
+	inputfile=str(sys.argv[1])
+	inp=open(inputfile,'r')
+	out=open("../Evolution2014Data/Corpus_StopRemoved.txt",'w')
+	for line in inp:
+		newline=""
+		newline=line.lower().strip()
+		for word in line.lower().split():
+			if stem(word) in stoplist or word.isdigit():
+				newline=re.sub(r'\b' + re.escape(word) + r'\b', '', newline)
+		newline=' '.join(newline.split())		
+		out.write(newline+"\n")	
+	out.close()
+	inp.close()
 
-	def filterstopwords():
-		inputfile=str(sys.argv[1])
-		inp=open(inputfile,'r')
-		out=open("../Evolution2014Data/Corpus_StopRemoved.txt",'w')
-		for line in inp:
-			newline=""
-			newline=line.lower().strip()
-			for word in line.lower().split():
+def getkeywords(notopics,corpus_lsi,lsi,stem2word):
+	#See here for more info: http://radimrehurek.com/gensim/tut2.html
+	count=1
 
-				if word in stoplist:
-					
-					newline=re.sub(r'\b' + re.escape(word) + r'\b', '', newline)
-			newline=' '.join(newline.split())		
-			out.write(newline+"\n")	
-		out.close()
-		inp.close()	
+	# to get topics list for all 100 topics 
+	#lsi.print_topics(100)
 
-
-	def getkeywords():
-		#See here for more info: http://radimrehurek.com/gensim/tut2.html
-		count=1
-
-		# to get topics list for all 100 topics 
-		#lsi.print_topics(100)
-
-		writetopic=open("../Evolution2014Data/TopicWords_"+str(notopics)+".txt",'w')
-		for doc in corpus_lsi: 
-			
-			keywords=set()
-			doc.sort(key=lambda tup: tup[1], reverse=True)
-			#print "top1",doc[0][0] # this is the top topic for this document
-			#print "top2",doc[1][0] # this is the next best topic for this document
-			i=0
-			
-			count+=1
-			while (i<4): #4 top topics
-				topicscore=lsi.show_topic(doc[i][0], topn=4)
-				# top four words from each topic
-
-				keywords.add(stem2word[topicscore[0][1].strip()])
-				keywords.add(stem2word[topicscore[1][1].strip()])
-				keywords.add(stem2word[topicscore[2][1].strip()])
-				keywords.add(stem2word[topicscore[3][1].strip()])
-				i+=1
-
-			for word in keywords:
-				writetopic.write(word+",")
-			writetopic.write("\n")
+	writetopic=open("../Evolution2014Data/TopicWords_"+str(notopics)+".txt",'w')
+	for doc in corpus_lsi: 
 		
+		keywords=set()
+		doc.sort(key=lambda tup: tup[1], reverse=True)
+		#print "top1",doc[0][0] # this is the top topic for this document
+		#print "top2",doc[1][0] # this is the next best topic for this document
+		i=0
 		
-	filterstopwords()
+		count+=1
+		while (i<4): #4 top topics
+			topicscore=lsi.show_topic(doc[i][0], topn=4)
+			# top four words from each topic
+
+			keywords.add(stem2word[topicscore[0][1].strip()])
+			keywords.add(stem2word[topicscore[1][1].strip()])
+			keywords.add(stem2word[topicscore[2][1].strip()])
+			keywords.add(stem2word[topicscore[3][1].strip()])
+			i+=1
+
+		for word in keywords:
+			writetopic.write(word+",")
+		writetopic.write("\n")
+
+def main():
+
+	stoplist=createstoplist()
+	filterstopwords(stoplist)
 	inputfile="../Evolution2014Data/Corpus_StopRemoved.txt"
 	inputfile=str(sys.argv[1])
 	corpussize = sum(1 for line in open(inputfile))
 	notopics=str(sys.argv[2])
 
 	Scorematrix = [[0 for x in xrange(corpussize)] for x in xrange(corpussize)]
-
-
-
-
-	################### 
+ 
 	stem2word={}
 	for line in open(inputfile):
 		for word in line.lower().split():
@@ -84,13 +77,6 @@ def main():
 	dictionary.compactify() 
 	dictionary.save('./storeddictionary.dict') 
 
-
-	###################
-
-
-
-
-	###################### Corpus from a file
 
 	class MyCorpus(object):
 		def __iter__(self):
@@ -119,15 +105,10 @@ def main():
 	corpora.MmCorpus.serialize('./storedcorpus.mm', corpus_memory_friendly) 
 
 
-		###################
-
-
-		################### Load the stored dictionary and corpus
 
 	dictionary = corpora.Dictionary.load('./storeddictionary.dict')
 	corpus = corpora.MmCorpus('./storedcorpus.mm')
 
-		###################
 
 
 	# Creating Models here
@@ -141,7 +122,8 @@ def main():
 	#corpus_lsi = lsi[corpus]
 
 	#lsi.print_debug(num_topics=5, num_words=10)
-	################## LSI 
+
+ 
 	lsisimlist=[]
 	index = similarities.MatrixSimilarity(corpus_lsi)
 	row=0
@@ -167,7 +149,7 @@ def main():
 	# NOTE: Scores are in the 2D array Scorematrix. To get the similarity score between document x and document y, access Scorematrix[x-1][y-1]
 
 
-	getkeywords()	
+	getkeywords(notopics,corpus_lsi,lsi,stem2word)	
 
 
 if __name__ == "__main__":
