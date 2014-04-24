34a872e41e9832ebc5f5ee4303644e96d4207e79
prashanti <manda.prashanti@gmail.com>
2014-04-24 10:39:56 -0400

    Added code to print a list of abstractIDs for contributed presentations

--- src/createCorpus.py
+++ src/createCorpus.py
@@ -8,16 +8,22 @@
 def main():
 	inputfile=sys.argv[1]
 	out=open("/Users/pmanda/Documents/Python_NLP/Evolution2014Data/Evolution2014-Corpus.txt",'w')
+	awardfile=open("/Users/pmanda/Documents/Python_NLP/Evolution2014Data/Abstracts_awards.txt",'w')
+	abstractIDfile=open("/Users/pmanda/Documents/Python_NLP/Evolution2014Data/AbstractIDlist.txt",'w')
 	inp=open(inputfile,'r')
 	abstract2corpus={}
 	conversion=open("/Users/pmanda/Documents/Python_NLP/Evolution2014Data/AbstractID2CorpusID.tsv",'w')
 	corpusID=1
 	for line in inp:
 		presentationtype=line.split("\t")[1]
-		print line
+		award=line.split("\t")[5].strip()
+		abstractID=line.split("\t")[0].strip()
+		if "considered" in award:
+			awardfile.write(abstractID+"\n")
+
 		if presentationtype.strip() == "Contributed Presentation":
 
-			abstractID=line.split("\t")[0].strip()
+			abstractIDfile.write(abstractID+"\n")
 			abstract2corpus[corpusID]=int(abstractID)
 			corpusID+=1
 			title=line.split("\t")[2].strip()
@@ -27,9 +33,11 @@ def main():
 			abstract=abstract.replace("\r","")
 			data=title+" "+abstract
 			out.write(data+"\n")
+
 	conversion.write("CorpusID\tAbstractID" + "\n"  )
 	for corpusID in abstract2corpus:
 		conversion.write(str(corpusID)+"\t"+str(abstract2corpus[corpusID])+"\n")
+	awardfile.close()
 
 
 
