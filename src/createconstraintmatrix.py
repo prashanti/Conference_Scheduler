f10367237ce5339a1c2a93c7775d0a809d8b3c63
prashanti <manda.prashanti@gmail.com>
2014-04-27 12:06:32 -0400

    Fixed encoding of constraints in constraint matrix

--- src/createconstraintmatrix.py
+++ src/createconstraintmatrix.py
@@ -6,6 +6,20 @@ def populateabstractIDs():
 		abstractIDs.append(int(line.strip()))
 	return(abstractIDs)
 
+def addtoconstraints(i,constraints,session):
+	if i in constraints:
+		constraints[int(i)].add(session)
+	else:
+		constraints[int(i)]=set()
+		constraints[int(i)].add(session)
+	return constraints
+
+def printconstraintlength(constraintmatrix):
+	lengthout=open("../Evolution2014Data/ConstraintList.txt",'w')
+	for abstract in sorted(constraintmatrix, key=lambda k: len(constraintmatrix[k])):
+		lengthout.write("abstractID"+"\t"+str(abstract)+"\t"+str(len(constraintmatrix[abstract]))+"\n")
+	lengthout.close()
+
 def main():
 	awardfile=sys.argv[1]
 	award=open(awardfile,'r')
@@ -29,43 +43,66 @@ def main():
 	sessions.close()
 
 
-
 	constraints={}
 	temp_list=[]
 	i=1
 	
 	# Ernst Mayr award constraint - cannot be in last day
-
-
+	# Timeslots go from 0 to 15
+	# in the below mention where you can't put them
 	for session in sessioncodes:
 		for i in abstractIDs:
-
-			if i in awardabstracts: # this is an award abstract
+			if i == 347:
 				
-				if (session in timeslot_sessions[15]) or (session in timeslot_sessions[14]) or (session in timeslot_sessions[13]) or (session in timeslot_sessions[12]): # session is not safe for award abstract, do nothing
-					1
-
-				else: # session is safe for award abstract
-					if i in constraints:
-						constraints[int(i)].append(session)
-					else:
-						constraints[int(i)]=[]
-						constraints[int(i)].append(session)
-
-			else: # this is not an awards abstract, can schedule it in all sessions
+				if (session not in timeslot_sessions[10]) and (session not in timeslot_sessions[11]) and (session not in timeslot_sessions[12]) and (session not in timeslot_sessions[13]) and (session not in timeslot_sessions[14]) and (session not in timeslot_sessions[15]):
+						constraints=addtoconstraints(i,constraints,session)
+			elif i == 768:
 				
-				if i in constraints:
-					constraints[int(i)].append(session)
-				else:
-					constraints[int(i)]=[]
-					constraints[int(i)].append(session)
+				if (session not in timeslot_sessions[14]) and (session not in timeslot_sessions[15]):
+					constraints=addtoconstraints(i,constraints,session)
+
+
+			elif i == 837:
+				
+				if (session not in timeslot_sessions[0]) and (session not in timeslot_sessions[1]) and (session not in timeslot_sessions[2]) and (session not in timeslot_sessions[3]) and (session not in timeslot_sessions[4]) and (session not in timeslot_sessions[5]) and (session not in timeslot_sessions[6]) and (session not in timeslot_sessions[7]) : 
+						constraints=addtoconstraints(i,constraints,session)	
+
+			elif i == 1361:
+				
+				if (session not in timeslot_sessions[12]) and (session not in timeslot_sessions[13]) and (session not in timeslot_sessions[14]) and (session not in timeslot_sessions[15]): 
+						constraints=addtoconstraints(i,constraints,session)												
+			elif i == 349:
+				
+				if (session not in timeslot_sessions[12]) and (session not in timeslot_sessions[13]) and (session not in timeslot_sessions[14]) and (session not in timeslot_sessions[15]): 
+						constraints=addtoconstraints(i,constraints,session)
+
+			elif i == 776:
+				
+				if (session not in timeslot_sessions[8]) and (session not in timeslot_sessions[9]) and (session not in timeslot_sessions[10]) and (session not in timeslot_sessions[11]) and (session not in timeslot_sessions[12]) and (session not in timeslot_sessions[13]) and (session not in timeslot_sessions[14]) and (session not in timeslot_sessions[15]): 
+						#print "session",session
+						constraints=addtoconstraints(i,constraints,session)
+
+			elif i in awardabstracts: # this is an award abstract	
+				if (session not in timeslot_sessions[15]) and (session not in timeslot_sessions[14]): 
+					constraints=addtoconstraints(i,constraints,session)
+
+			else: #these abstracts have no constraints
+
+				constraints=addtoconstraints(i,constraints,session)
 					
-	for abstract in constraints:
-		print abstract,len(constraints[abstract])
+	
 
 
 	constraintsfile=open("../Evolution2014Data/ConstraintMatrix.txt",'w')
+	for x in constraints:
+		constraints[x]=list(constraints[x])
+		constraints[x].sort()
 	json.dump(constraints,constraintsfile)
+
+	printconstraintlength(constraints)
+
+	
+
 	constraintsfile.close()
 
 
@@ -73,5 +110,6 @@ def main():
 if __name__ == "__main__":
 	import sys
 	import json
+	from json import dumps, loads, JSONEncoder, JSONDecoder
 	from operator import itemgetter
 	main()
